ec5c6345052daf9aa80c36dc30e2c42ac45e368c
idoCo10 <ido.icohen10@gmail.com>
2025-12-11 00:22:43 +0700

    test

--- app/src/main/java/live/icenet/gymprogress/MainActivity.kt
+++ app/src/main/java/live/icenet/gymprogress/MainActivity.kt
@@ -25,10 +25,7 @@ import live.icenet.gymprogress.data.*
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
 
-
-
 // temp!
-
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
